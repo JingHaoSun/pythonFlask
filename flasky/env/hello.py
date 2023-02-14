32c17f0ab2e0e882b8b02019922e40d713d86411
JingHaoSun <2806809869@qq.com>
2023-02-14 08:52:47 +0800

    20230214am8

--- flasky/env/hello.py
+++ flasky/env/hello.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, render_template, flash
 from flask import abort
 from datetime import datetime
@@ -6,7 +8,9 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from flask_sqlalchemy import SQLAlchemy
 from wtforms.validators import DataRequired
+from gevent import pywsgi
 
 
 
@@ -15,10 +19,37 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
+#数据库配置sqllite
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+ 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# server = pywsgi.WSGIServer(('0.0.0.0', 12345), app)
+# server.serve_forever()
+
 
 class NameForm(FlaskForm):
  name = StringField('What is your name?', validators=[])
  submit = SubmitField('Submit')
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role',lazy='dynamic')
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -27,11 +58,26 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        #变更名字提示
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form,  name=session.get('name'), current_time=datetime.utcnow())
+
+    #current_time(时间显示)
+    return render_template('index.html',
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False),current_time=datetime.utcnow())
 
 
 
